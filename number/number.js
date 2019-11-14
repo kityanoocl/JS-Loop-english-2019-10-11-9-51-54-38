bc551bed6edc754769d32e8f4e35b537ab87c9b9
Kit Yan <kit.yan@oocl.com>
2019-11-14 09:51:42 +0800

    Added functions in number.js and multiplication.js

---
+++ number/number.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++)
+{
+    if (i % 2 == 0)
+    {
+        document.write(i + ' is even number.<br>');
+    }
+    else
+    {
+        document.write(i + ' is odd number.<br>');
+    }
+}
